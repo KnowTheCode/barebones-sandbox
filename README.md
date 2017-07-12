54c702957455d05bb9ea0a2f3c087bc10e25b7b6
hellofromtonya <hellofromtonya@knowthecode.io>
2017-07-12 17:01:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Barebones Sandbox Plugin
+
+This repository is a barebones Sandbox WordPress plugin that you can use as a starting point for doing hands-on labs on [Know the Code](https://KnowTheCode/library).
+
+## Installation
+
+1. In terminal, navigate to `{path to your sandbox project}/wp-content/plugins`.
+2. Then type in terminal (or Git Bash): `git clone https://github.com/KnowTheCode/barebones-sandbox.git`.
+3. Press enter or return.  The plugin starts cloning into your project.
+4. When it's done, log into your WordPress website.
+5. Go to Plugins and activate the Barebones Sandbox plugin.
---
+++ bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Barebones Sandbox Plugin
+ *
+ * @package     KnowTheCode\BarebonesSandbox
+ * @author      hellofromTonya
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Barebones Sandbox Plugin
+ * Plugin URI:  https://github.com/KnowTheCode/barebones-sandbox
+ * Description: Barebones sandbox Testing plugin where we can write code, reverse engineer, and break stuff to learn deeply about WordPress web development.
+ * Version:     1.0.0
+ * Author:      hellofromTonya
+ * Author URI:  https://KnowTheCode.io
+ * Text Domain: sandbox
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+namespace KnowTheCode\BarebonesSandbox;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Cheatin&#8217; uh?' );
+}
+
+// add your cool testing and playing code here.  Have fun!
